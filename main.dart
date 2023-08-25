8fa0255e09ac492f0036823d55fe12f69480684d
Sheyenne York <support@shopwindow.io>
2023-08-25 14:45:06 -0400

    Add initial sprint to port to Dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import "dart:io";
+import "dart:math";
+import "package:image/image.dart" as img;
+import "imagecaptcha.dart";
+
+void main()
+{
+  ImageCaptcha captcha = new ImageCaptcha(80, 30);
+
+  //p.color(0, 0, 0, 0);  // First color: background (red, green, blue, alpha)
+  //p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha)
+
+  double rand = new Random().nextDouble();
+
+  img.Image image = captcha.render( (rand*9000+1000).round() );
+
+  new File("test2.png").writeAsBytes( img.encodePng(image) );
+}
+
